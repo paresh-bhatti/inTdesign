9263902946cb50d892fd779d06b646a39f3638db
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-04-04 12:13:37 -0400

    test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,24 +9,46 @@ var client = yelp.client(yelpKey);
 var db = require("../models");
 console.log(yelpKey);
 
+
 module.exports = function (app) {
 
     app.get("/api/yelp/:businessName/:city", function (req, res) {
         var name = req.params.businessName;
         
         var city = req.params.city;
+
+        var request = require("request");
+
+        var options = {
+            method: 'GET',
+            url: 'https://api.yelp.com/v3/businesses/search',
+            qs: { term: name, location: city },
+            headers:
+            {
+                'Postman-Token': '49aeea3f-8ce1-4b18-a6ff-d446b3a36267',
+                'cache-control': 'no-cache',
+                Authorization: 'Bearer 6mOLysbB4TWzxVVj53g49Xjlq-pH4r8wrkdguf3gUtdl9irTVoT4EKJWjhCmdT2_yV558KNdUkDQDmKugwWExwEbdO9jYg2yQanVWmxULR4naCadfCBuSU79TWyWXHYx'
+            }
+        };
+
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            var results = JSON.parse(body);
+            console.log(results);
+            res.send(results);
+        });
+
         
-        client.search({
-            term: name,
-            location: city
-        }).then(response => {
+        // client.search({
+        //     term: name,
+        //     location: city
+        // }).then(response => {
             
-            var results = response.jsonBody.businesses;
+        //     console.log(response);
+        //     var results = response.jsonBody.businesses;
             
-            res.json(results);
-
-
-        }).catch(error => { console.log(error) });
+        //     res.json(results);
+        // }).catch(error => { console.log(error) });
     });
 
     app.get("/api/yelp/:specificId", function (somereq, res) {
